39b047712198fb6fcf00d35c5136500fbec4c944
npark <npark@amazon.com>
2020-03-19 22:19:52 -0400

    minor syntax fixes

--- CisScanningLambda.py
+++ CisScanningLambda.py
@@ -1,4 +1,3 @@
-
 #
 # This file made available under CC0 1.0 Universal (https://creativecommons.org/publicdomain/zero/1.0/legalcode)
 #
@@ -14,21 +13,15 @@ from botocore.exceptions import ClientError
 def evaluate_compliance(configuration_item):
     region     = configuration_item['awsRegion']
     ssm_client = boto3.client('ssm', region_name=region)
-    try:
-      instanceId = configuration_item['configuration']['instanceId']
-    except TypeError:
-      return {
-        "compliance_type": "NOT_APPLICABLE",
-        "annotation": "configurationItem array is empty"
-      }
+    instance_id = configuration_item['configuration']['instanceId']
 
     ssm_response = ssm_client.send_command(
-             InstanceIds=[ instanceId ],
-             DocumentName="AWS-RunInspecChecks",
-             Parameters={
-                'sourceInfo':[ '{ "owner":"dev-sec", "repository":"cis-dil-benchmark", "path": "", "getOptions" : "branch:master", "tokenInfo":"{{ssm-secure:github-personal-token-InSpec}}" }' ],
-                'sourceType': [ 'GitHub' ]
-             })
+        InstanceIds=[ instance_id ],
+        DocumentName="AWS-RunInspecChecks",
+        Parameters={
+            'sourceInfo':[ '{ "owner":"dev-sec", "repository":"cis-dil-benchmark", "path": "", "getOptions" : "branch:master", "tokenInfo":"{{ssm-secure:github-personal-token-InSpec}}" }' ],
+            'sourceType': [ 'GitHub' ]
+        })
     command_id = ssm_response['Command']['CommandId']
 
     ctr=0
@@ -36,7 +29,7 @@ def evaluate_compliance(configuration_item):
         try:
             output = ssm_client.get_command_invocation(
                 CommandId=command_id,
-                InstanceId=instanceId,
+                InstanceId=instance_id,
                 PluginName='runInSpecLinux'
                 )
             status = output['Status']
@@ -45,14 +38,14 @@ def evaluate_compliance(configuration_item):
                 message = output['StandardOutputContent']
                 x = re.search("and 0 non-compliant",message)
                 if not x:
-                    annotation = "The ec2 instance " + instanceId +" is NOT compliant"
+                    annotation = "The ec2 instance " + instance_id +" is NOT compliant"
                     compliance_type = 'NON_COMPLIANT'
                 else:
-                    annotation = "The ec2 instance " + instanceId +" is compliant"
+                    annotation = "The ec2 instance " + instance_id +" is compliant"
                     compliance_type = 'COMPLIANT'
                 print (annotation)
             elif status == 'Delivery Timed Out' or status == 'Execution Timed Out' or status == 'Failed' or status == 'Canceled' or status == 'Undeliverable' or status == 'Terminated':
-                annotation = "cis-dil-benchmark scan was not successful. Ec2 instance " + instanceId + "'s state could not be determined.  Marked NON_COMPLIANT for now."
+                annotation = "cis-dil-benchmark scan was not successful. Ec2 instance " + instance_id + "'s state could not be determined.  Marked NON_COMPLIANT for now."
                 compliance_type = 'NON_COMPLIANT'
             break
         except ClientError as e:
@@ -66,19 +59,19 @@ def evaluate_compliance(configuration_item):
 
 def lambda_handler(event, context):
     print ("event: ",event)
-    invoking_event      = json.loads(event['invokingEvent'])
-    configuration_item  = invoking_event["configurationItem"]
+    invoking_event     = json.loads(event['invokingEvent'])
+    configuration_item = invoking_event["configurationItem"]
     if configuration_item['resourceType'] != "AWS::EC2::Instance":
         print ("DEBUG: I can only evaluate EC2 instances")
         evaluation = {
             "compliance_type": "NOT_APPLICABLE",
             "annotation": "Wrong resource type"
         }
-    elif configuration_item['configuration']['instanceId'] == None:
-        print("DEBUG: instandId missing")
+    elif configuration_item['configuration'] == None or configuration_item['configuration']['instanceId'] == None:
+        print("DEBUG: cannot retrieve instanceId")
         evaluation = {
             "compliance_type": "INSUFFICIENT_DATA",
-            "annotation": "configurationItem array is empty"
+            "annotation": "configurationItem array is empty or instanceId missing"
         };
     else:
         evaluation = evaluate_compliance(configuration_item)
